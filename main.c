7177ed04ecfe596b3c01fc4868cd52571962aae7
はるでら <43818636+springtemple@users.noreply.github.com>
2018-12-20 14:22:03 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -4,26 +4,30 @@
 
 struct Question{
 	int num;
-	char qname[100];
 	char q[100];
 	char ans[100];
 }Q[100];
 
-int total;
+int total;		//問題数
+int rondom;		//出題順
+int bad;		//不正解の処理
 
 void Read();
-void Answer();
-void Seitouritu();
+void Config();
+void ConfigRead();
 
 int main()
 {
 	int a;
 	
+	printf("単語帳プログラム\n\n");
+	
 	while(a != 0){
-		printf("---単語帳プログラム---\n\n");
+		printf("---初期画面---\n\n");
 		printf("問題登録 -> 1\n");
 		printf("解答する -> 2\n");
 		printf("辞書 -> 3\n");
+		printf("設定 -> 4\n");
 		printf("終了 -> 0\n");
 		printf("-> ");
 		scanf("%d",&a);
@@ -33,7 +37,7 @@ int main()
 				
 				break;
 			case 2:
-				Answer();
+			
 				break;
 			case 3:
 				
@@ -64,7 +68,6 @@ void Read(){
 	fi = fopen(filename,"r");
 	i = 0;
 	while(feof(fi) == 0){
-		fscanf(fi,"%s",Q[i].qname);
 		fscanf(fi,"%s",Q[i].q);
 		fscanf(fi,"%s",Q[i].ans);
 		i++;
@@ -74,18 +77,14 @@ void Read(){
 }
 
 void ConfigRead(){
-	*fw;
+	FILE *fi;
 	
 	fi = fopen("config.txt","r");
-	fscanf("%d",&rondom);
-	fscanf("%d",&bad);
+	fscanf(fi,"%d",&rondom);
+	fscanf(fi,"%d",&bad);
 	fclose(fi);
-}
-
-void Config(){
-	int rondom,bad,a;
-	FILE *fi,
-	printf("---設定---\n");
+	
+	printf("---設定---\n\n");
 	printf("現在の設定\n");
 	printf("出題順：");
 	switch(rondom){
@@ -106,19 +105,46 @@ void Config(){
 			break;
 	}
 	printf("\n");
-	printf("出題順の変更 -> 1\n");
-	printf("間違った問題の設定 -> 2\n");
-	scanf("%d",&a);
-	case(a){
-		case 1:
-			printf("順番に出題する -> 1\n");
-			printf("ランダムに出題する -> 2\n");
-			break;
-		case 2:
-			printf("間違った問題を最後にすべて出力する -> 0\n");
-			printf("解けるまで解き直す-> 1\n");
-			break;
-	}
+}	
+
+void Config(){
+	int a;
+	FILE *fw;
+	
+	ConfigRead();
+	do{
+		printf("出題順の変更 -> 1\n");
+		printf("間違った問題の設定 -> 2\n");
+		printf("終了する -> 0\n");
+		printf("-> ");
+		scanf("%d",&a);
+		fw = fopen("config.txt","w");
+		switch(a){
+			case 1:
+				printf("順番に出題する -> 0\n");
+				printf("ランダムに出題する -> 1\n");
+				printf("-> ");
+				scanf("%d",&rondom);
+				printf("変更しました\n");
+				break;
+			case 2:
+				printf("間違った問題を最後にすべて出力する -> 0\n");
+				printf("解けるまで解き直す-> 1\n");
+				printf("-> ");
+				scanf("%d",&bad);
+				printf("変更しました\n");
+				break;
+			case 0:
+				break;
+			default:
+				printf("違う文字が入力されました。\n");
+				printf("もう一度入力しなおしてください\n");
+				break;
+		}
+		fprintf(fw,"%d\n%d",rondom,bad);
+		fclose(fw);
+	}while(a != 0);
+	printf("\n");
 }
 	
 
