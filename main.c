d36d491e6c55aeefc086147fde6bc9bd0eefdd80
はるでら <43818636+springtemple@users.noreply.github.com>
2019-01-10 14:16:37 +0900

    main.c
    体裁を整えました

--- main.c
+++ main.c
@@ -4,39 +4,54 @@
 
 struct Question{
 	int num;
-	char qname[100];
 	char q[100];
 	char ans[100];
 }Q[100];
 
-int total;
+int total;		//問題数
+int rondom;		//出題順
+int bad;		//不正解の処理
 
 void Read();
-void Answer();
-void Seitouritu();
+void Config();
+void ConfigRead();
 
 int main()
 {
 	int a;
 	
+	printf("単語帳プログラム\n\n");
+	
 	while(a != 0){
-		printf("---単語帳プログラム---\n\n");
+		printf("---初期画面---\n\n");
 		printf("問題登録 -> 1\n");
 		printf("解答する -> 2\n");
-		printf("逆引き -> 3\n");
+		printf("辞書 -> 3\n");
+		printf("設定 -> 4\n");
 		printf("終了 -> 0\n");
 		printf("-> ");
 		scanf("%d",&a);
 		printf("\n");
 		switch(a){
 			case 1:
-			
+				printf("\x1b[31m");	//圓福モジュール（赤）
+				
+				printf("\x1b[0m");
 				break;
 			case 2:
-				Answer();
+				printf("\x1b[33m");	//梶田モジュール（黄）
+				
+				printf("\x1b[0m");	
 				break;
 			case 3:
-			
+				printf("\x1b[35m");	//花谷モジュール（マゼンタ）
+				
+				printf("\x1b[0m");
+				break;
+			case 4:
+				printf("\x1b[36m");	//木村モジュール（シアン）
+				Config();
+				printf("\x1b[0m");
 				break;
 			case 0:
 				printf("終了します。お疲れ様でした。\n");
@@ -49,25 +64,3 @@ int main()
 	}
 	return 0;
 }
-
-void Read(){
-	int i;
-	char filename[100];
-	FILE *fi;
-	
-	printf("読み込むジャンルを選択してください\n");
-	printf("-> ");
-	scanf("%s",filename);
-	fi = fopen(filename,"r");
-	i = 0;
-	while(feof(fi) == 0){
-		fscanf(fi,"%s",Q[i].qname);
-		fscanf(fi,"%s",Q[i].q);
-		fscanf(fi,"%s",Q[i].ans);
-		i++;
-	}
-	total = i - 1;
-	fclose(fi);
-}
-	
-
